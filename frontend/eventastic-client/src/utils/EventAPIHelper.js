b3729e591deea76531bafdb67f54b424dd7dcba6
llricho <z5188286@ad.unsw.edu.au>
2022-06-24 15:20:16 +1000

    Added functionality to integrate Home Page with backend API.

---
+++ frontend/eventastic-client/src/utils/EventAPIHelper.js
@@ -0,0 +1,30 @@
+import * as axios from "axios";
+
+export default class EventAPI {
+  constructor() {
+    this.api_token = null;
+    this.client = null;
+    this.api_url = 'http://127.0.0.1:8081/v1';
+  }
+
+  init = () => {
+
+    let headers = {
+      Accept: "application/json",
+    };
+
+    this.client = axios.create({
+      baseURL: this.api_url,
+      timeout: 31000,
+      headers: headers,
+    });
+
+    return this.client;
+  };
+
+  getEventList = () => {
+    return this.init().get("/events");
+  };
+
+  // Add additional API call here as needed ...
+}
