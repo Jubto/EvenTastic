a6373a953be02897058282ff59595d01707dfce1
Sai Bhargav Kommuru <sai.15bhargav@gmail.com>
2022-06-24 18:37:52 +1000

    Venue controller fetch api based on venue_id done

--- services/events-service/python-flask-server/swagger_server/controllers/venues_controller.py
+++ services/events-service/python-flask-server/swagger_server/controllers/venues_controller.py
@@ -37,14 +37,12 @@ def create_venue(body):  # noqa: E501
         for seating in body.seating:
             cur.execute(f"INSERT INTO venue_seating values (default,{body.venue_id},'{seating.seating_type}',{seating.seating_number});")
 
-        cur.close()
-        con.close()
+        return body, 201, {'Access-Control-Allow-Origin': '*'}
     except Exception as err:
         error = UnexpectedServiceError(code="500", type="UnexpectedServiceError", message=str(err))
+        cur.close()
+        con.close()
         return error, 500, {'Access-Control-Allow-Origin': '*'}
-
-    return body, 201, {'Access-Control-Allow-Origin': '*'}
-
 
 def get_venue_details(venue_id):  # noqa: E501
     """Retrieve Venue details by Venue ID.
@@ -56,7 +54,41 @@ def get_venue_details(venue_id):  # noqa: E501
 
     :rtype: Venue
     """
-    return 'do some magic!'
+    try:
+        # attempt to get Venue from the store/database
+        port = 49157
+        con = psycopg2.connect(database= 'eventastic', user='postgres', password='postgrespw', host="localhost", port=port)
+        con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = con.cursor()
+        cur.execute(f"select * from venues where venue_id={venue_id}")
+        venue = cur.fetchone()
+        if venue == None:
+            # return 401 if error if Venue does not exist
+            cur.close()
+            con.close()
+            return VenueNotFoundError(
+                code=401, type="VenueNotFoundError", 
+                message="The following Venue does not exist: " + str(venue_id)), 401, {'Access-Control-Allow-Origin': '*'}
+        
+        cur.execute(f"select seating_type, seating_number from venue_seating where venue_id={venue_id}")
+        venue_seatings = cur.fetchall()
+        for i in range(len(venue_seatings)):
+            seat_record = venue_seatings[i]
+            venue_seatings[i] = {'seating_type':seat_record[0],'seating_number':seat_record[1]}
+        
+        venue = {'seating':venue_seatings,'venue_id':venue[0], 'venue_name':venue[1], 'venue_desc':venue[2], 'venue_img':venue[3], 'venue_address':venue[4] }
+        
+        cur.close()
+        con.close()
+            
+        # If successful, return the Venue object
+        return venue, 200, {'Access-Control-Allow-Origin': '*'}
+    except Exception as e:
+        # catch any unexpected runtime error and return as 500 error 
+        error = UnexpectedServiceError(code="500", type="UnexpectedServiceError", message=str(e))
+        cur.close()
+        con.close()
+        return error, 500, {'Access-Control-Allow-Origin': '*'}
 
 
 def list_venues(venue_name=None):  # noqa: E501
