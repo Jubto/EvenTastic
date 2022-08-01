2f5e269ef4301bf002d29401d97cefa87d0c99e8
Sai Bhargav Kommuru <sai.15bhargav@gmail.com>
2022-08-02 03:33:33 +1000

    test schedule done

--- database/scheduler.py
+++ database/scheduler.py
@@ -22,30 +22,36 @@ def call_me():
         end_time = row[13][:-6]
         end_time = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S')
         event_id = row[0]
-        if CurrentDate > end_time:
-            print(f"Event:{event_id} is completed")
-            cur.execute(
-                f"Update events SET event_status='COMPLETED' where event_id={event_id};")
-
+        start_time = row[12][:-6]
+        start_time = datetime.datetime.strptime(
+            start_time, '%Y-%m-%dT%H:%M:%S')
+        event_name = row[8]
+        if CurrentDate > start_time:
             cur.execute(
                 f"Update bookings SET booking_status='Completed' where event_id={event_id} and booking_status='Booked';")
-            cur.execute(
-                f"Select * from rewardpoints where event_id={event_id};")
-            rewards = cur.fetchall()
-            for reward in rewards:
-                reward_id = reward[0]
-                account_id = reward[1]
+            if CurrentDate > end_time:
+                print(f"Event:{event_name} is completed")
                 cur.execute(
-                    f"Select reward_points from accounts where account_id={account_id}")
-                prev_reward_point = float(cur.fetchall()[0])
+                    f"Update events SET event_status='COMPLETED' where event_id={event_id};")
                 cur.execute(
-                    f"Update rewardpoints SET reward_points_status='Approved' where reward_points_id={reward_id}")
-                cur.execute(
-                    f"Select reward_points_amount from rewardpoints where reward_points_id={reward_id}")
-                new_reward_points = prev_reward_point + \
-                    float(cur.fetchall()[0])
-                cur.execute(
-                    f"Update accounts SET reward_points={new_reward_points} where account_id={account_id}")
+                    f"Select * from rewardpoints where event_id={event_id};")
+                rewards = cur.fetchall()
+                for reward in rewards:
+                    reward_id = reward[0]
+                    account_id = reward[1]
+                    cur.execute(
+                        f"Select reward_points from accounts where account_id={account_id}")
+                    prev_reward_point = float(cur.fetchall()[0])
+                    cur.execute(
+                        f"Update rewardpoints SET reward_points_status='Approved' where reward_points_id={reward_id}")
+                    cur.execute(
+                        f"Select reward_points_amount from rewardpoints where reward_points_id={reward_id}")
+                    new_reward_points = prev_reward_point + \
+                        float(cur.fetchall()[0])
+                    cur.execute(
+                        f"Update accounts SET reward_points={new_reward_points} where account_id={account_id}")
+                    print(
+                        f"\tAccount-id:{account_id} ==> Previous Reward Points:{prev_reward_point} Current Reward Points:{new_reward_points}")
 
     cur.close()
     con.close()
